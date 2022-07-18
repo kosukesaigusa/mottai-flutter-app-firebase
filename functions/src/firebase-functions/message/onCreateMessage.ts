b5ec5a96a70f8b4105a98220fb70a256cd910018
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-07-19 07:31:52 +0900

    refactor: refactor onCreateMessage realted codes

--- functions/src/firebase-functions/message/onCreateMessage.ts
+++ functions/src/firebase-functions/message/onCreateMessage.ts
@@ -8,7 +8,7 @@ import { messageConverter } from '../../../src/converters/messageConverter'
 import { MessageRepository } from '../../repositories/message'
 import { attendingRoomRef } from '../../firestore-refs/firestoreRefs'
 
-/** ルームに新しいメッセージが作成されたときに発火する Function。*/
+/** チャットルームに新しいメッセージが作成されたときに発火する。*/
 export const onCreateMessage = functions
     .region(`asia-northeast1`)
     .firestore.document(`message/{v1Message}/rooms/{roomId}/messages/{messageId}`)
@@ -25,88 +25,124 @@ export const onCreateMessage = functions
         }
         const hostId = room.hostId
         const workerId = room.workerId
-        const hostAttendingRoomRef = attendingRoomRef({ userId: hostId, roomId: roomId })
-        const workerAttendingRoomRef = attendingRoomRef({ userId: workerId, roomId: roomId })
-        const hostAttendingRoomDs = await hostAttendingRoomRef.get()
-        const workerAttendingRoomDs = await workerAttendingRoomRef.get()
-        const hostAttendingRoom = hostAttendingRoomDs.data()
-        const workerAttendingRoom = workerAttendingRoomDs.data()
+        const hostAttendingRoom = await messageRepository.fetchAttendingRoom({ userId: hostId, roomId: roomId })
+        const workerAttendingRoom = await messageRepository.fetchAttendingRoom({ userId: workerId, roomId: roomId })
 
-        // ルーム参加者のそれぞれの AttendingRoom バッチ書き込みする。
-        // すでに AttendingRoom が存在していれば updatedAt と unreadCount を更新する。
-        // 存在しなければ新たに set する。
-        const batch = admin.firestore().batch()
-        if (hostAttendingRoom !== undefined) {
-            batch.update(hostAttendingRoomRef, {
-                updatedAt: FieldValue.serverTimestamp(),
-                unreadCount: FieldValue.increment(1)
-            })
-        } else {
-            batch.set(
-                hostAttendingRoomRef,
-                attendingRoomConverter.toFirestore({
-                    roomId,
-                    partnerId: workerId,
-                    unreadCount: 0,
-                    muteNotification: false,
-                    isBlocked: false,
-                    lastReadMessageId: ``
-                })
-            )
-        }
-        if (workerAttendingRoom !== undefined) {
-            batch.update(workerAttendingRoomRef, {
-                updatedAt: FieldValue.serverTimestamp(),
-                unreadCount: FieldValue.increment(1)
-            })
-        } else {
-            batch.set(
-                workerAttendingRoomRef,
-                attendingRoomConverter.toFirestore({
-                    roomId,
-                    partnerId: hostId,
-                    unreadCount: 0,
-                    muteNotification: false,
-                    isBlocked: false,
-                    lastReadMessageId: ``
-                })
-            )
-        }
+        // チャットルーム参加者のそれぞれの AttendingRoom を更新する。
         try {
-            await batch.commit()
-            functions.logger.info(`onCreateMessage による AttendingRoom の更新に成功しました`)
+            writeAttendingRoom({ hostAttendingRoom, workerAttendingRoom, room: room })
+            functions.logger.info(`onCreateMessage による AttendingRoom の更新に成功しました。`)
         } catch (e) {
-            functions.logger.error(`onCreateMessage のバッチ書き込みに失敗しました：${e}`)
+            functions.logger.error(`onCreateMessage のバッチ書き込みに失敗しました。：${e}`)
             return
         }
 
-        // 通知を送る対象を確認する
-        const userIds: string[] = []
-        if (senderId === hostId) {
-            const cannotSendFCM =
-                (workerAttendingRoom?.isBlocked ?? false) || (workerAttendingRoom?.muteNotification ?? false)
-            if (!cannotSendFCM) {
-                userIds.push(workerId)
-            }
-        } else if (senderId === workerId) {
-            const cannotSendFCM =
-                (hostAttendingRoom?.isBlocked ?? false) || (hostAttendingRoom?.muteNotification ?? false)
-            if (!cannotSendFCM) {
-                userIds.push(workerId)
-            }
-        }
-        if (userIds.length === 0) {
-            functions.logger.info(`通知の送信対象が存在しませんでした`)
-            return
-        }
-        const title = sender.displayName
-        const body = message.body
-        const path = `/room`
+        // 対象者に通知を送る。
         try {
-            await sendFCMByUserIds({ userIds, title, body, path })
-            functions.logger.info(`onCreateMessage による通知の送信に成功しました`)
+            await sendNotificationToReceiver({ hostAttendingRoom, workerAttendingRoom, room, message, sender })
+            functions.logger.info(`onCreateMessage による通知の送信に成功しました。`)
         } catch (e) {
-            functions.logger.error(`通知の送信に失敗しました：${e}`)
-            return
+            functions.logger.error(`通知の送信に失敗しました。：${e}`)
         }
     })
+
+/**
+ * チャットルームに参加しているそれぞれのユーザーの AttendingRoom をバッチ書き込みをする。
+ * すでに AttendingRoom が存在していれば updatedAt と unreadCount を更新する。
+ * 存在しなければ新たに AttendingRoom ドキュメントを set する。
+ */
+const writeAttendingRoom = async ({
+    hostAttendingRoom,
+    workerAttendingRoom,
+    room
+}: {
+    hostAttendingRoom: AttendingRoom | undefined
+    workerAttendingRoom: AttendingRoom | undefined
+    room: Room
+}): Promise<void> => {
+    const hostId = room.hostId
+    const workerId = room.workerId
+    const roomId = room.roomId
+
+    const hostAttendingRoomRef = attendingRoomRef({ userId: hostId, roomId: roomId })
+    const workerAttendingRoomRef = attendingRoomRef({ userId: workerId, roomId: roomId })
+
+    const batch = admin.firestore().batch()
+    if (hostAttendingRoom !== undefined) {
+        batch.update(hostAttendingRoomRef, {
+            updatedAt: FieldValue.serverTimestamp(),
+            unreadCount: FieldValue.increment(1)
+        })
+    } else {
+        batch.set(
+            hostAttendingRoomRef,
+            attendingRoomConverter.toFirestore({
+                roomId: roomId,
+                partnerId: workerId,
+                unreadCount: 0,
+                muteNotification: false,
+                isBlocked: false,
+                lastReadMessageId: ``
+            })
+        )
+    }
+    if (workerAttendingRoom !== undefined) {
+        batch.update(workerAttendingRoomRef, {
+            updatedAt: FieldValue.serverTimestamp(),
+            unreadCount: FieldValue.increment(1)
+        })
+    } else {
+        batch.set(
+            workerAttendingRoomRef,
+            attendingRoomConverter.toFirestore({
+                roomId: roomId,
+                partnerId: hostId,
+                unreadCount: 0,
+                muteNotification: false,
+                isBlocked: false,
+                lastReadMessageId: ``
+            })
+        )
+    }
+    await batch.commit()
+}
+
+/** 対象者に FCM の通知を送る。*/
+const sendNotificationToReceiver = async ({
+    hostAttendingRoom,
+    workerAttendingRoom,
+    room,
+    message,
+    sender
+}: {
+    hostAttendingRoom: AttendingRoom | undefined
+    workerAttendingRoom: AttendingRoom | undefined
+    room: Room
+    message: Message
+    sender: PublicUser
+}): Promise<void> => {
+    const senderId = message.senderId
+    const hostId = room.hostId
+    const workerId = room.workerId
+    const userIds: string[] = []
+    if (senderId === hostId) {
+        const cannotSendFCM =
+            (workerAttendingRoom?.isBlocked ?? false) || (workerAttendingRoom?.muteNotification ?? false)
+        if (!cannotSendFCM) {
+            userIds.push(workerId)
+        }
+    } else if (senderId === workerId) {
+        const cannotSendFCM = (hostAttendingRoom?.isBlocked ?? false) || (hostAttendingRoom?.muteNotification ?? false)
+        if (!cannotSendFCM) {
+            userIds.push(workerId)
+        }
+    }
+    if (userIds.length === 0) {
+        functions.logger.info(`通知の送信対象が存在しませんでした`)
+        return
+    }
+    const title = sender.displayName
+    const body = message.body
+    const path = `/room`
+    await sendFCMByUserIds({ userIds, title, body, path })
+}
